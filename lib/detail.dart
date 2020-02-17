44e0276683f89339cba03afbe7f9d21d6bc19eb7
hafiznohaimi <hafiznohaimi@users.noreply.github.com>
2020-02-17 15:28:38 +0800

    flutter project 2

---
+++ lib/detail.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+
+  final String name;
+  final String email;
+  final String phone;
+
+  const DetailPage(this.name, this.email, this.phone);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Detail Page")
+      ),
+      body: Container(
+        // child: Text("This is second page"),
+          child: Column(
+            children: <Widget>[
+              Text('${widget.name}'),
+              Text('${widget.phone}'),
+              Text('${widget.email}')
+            ],
+          )
+      ),
+    );
+  }
+}
